8874f1b90fc260e67ae950be03dfb1e059d4a2fb
piudasgupta42 <piudasgupta346@gmail.com>
2026-02-06 17:13:43 +0530

    Update script.js

--- WEEK 3/script.js
+++ WEEK 3/script.js
@@ -1,69 +1,26 @@
 
 function getWeather() {
-  const city = document.getElementById("city").value;
-  const output = document.getElementById("output");
+  var city = document.getElementById("city").value;
+  var result = document.getElementById("result");
 
-  const API_KEY = "function getWeather() {
-  const city = document.getElementById("city").value;
-  const output = document.getElementById("output");
+  if (city === "") {
+    result.innerText = "Please enter a city name";
+    return;
+  }
 
-  const API_KEY = "https://api.openweathermap.org/data/2.5/weather";
-
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city + "&units=metric&appid=" + API_KEY;
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-
-  xhr.onload = function () {
-    if (xhr.status !== 200) {
-      output.innerText = "Error: " + xhr.status;
-      return;
-    }
-
-    const data = JSON.parse(xhr.responseText);
-
-    output.innerText =
-      "City: " + data.name + "\n" +
-      "Temperature: " + data.main.temp + " °C\n" +
-      "Humidity: " + data.main.humidity + "%\n" +
-      "Condition: " + data.weather[0].description;
+  // Simulated API response (JSON)
+  var weatherData = {
+    city: city,
+    temperature: 29,
+    humidity: 70,
+    condition: "Clear Sky"
   };
 
-  xhr.onerror = function () {
-    output.innerText = "Network error";
-  };
-
-  xhr.send();
-}";
-
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city + "&units=metric&appid=" + API_KEY;
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-
-  xhr.onload = function () {
-    if (xhr.status !== 200) {
-      output.innerText = "Error: " + xhr.status;
-      return;
-    }
-
-    const data = JSON.parse(xhr.responseText);
-
-    output.innerText =
-      "City: " + data.name + "\n" +
-      "Temperature: " + data.main.temp + " °C\n" +
-      "Humidity: " + data.main.humidity + "%\n" +
-      "Condition: " + data.weather[0].description;
-  };
-
-  xhr.onerror = function () {
-    output.innerText = "Network error";
-  };
-
-  xhr.send();
+  // DOM manipulation + JSON usage
+  result.innerHTML =
+    "<h3>Weather Details</h3>" +
+    "<b>City:</b> " + weatherData.city + "<br>" +
+    "<b>Temperature:</b> " + weatherData.temperature + " °C<br>" +
+    "<b>Humidity:</b> " + weatherData.humidity + "%<br>" +
+    "<b>Condition:</b> " + weatherData.condition;
 }
-   
