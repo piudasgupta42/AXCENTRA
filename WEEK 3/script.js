32500738a5d8b03ea8e6b89c8b06f92e96369845
piudasgupta42 <piudasgupta346@gmail.com>
2026-02-06 16:51:20 +0530

    Update script.js

--- WEEK 3/script.js
+++ WEEK 3/script.js
@@ -3,7 +3,40 @@ function getWeather() {
   const city = document.getElementById("city").value;
   const output = document.getElementById("output");
 
-  const API_KEY = "https://openweathermap.org/api";
+  const API_KEY = "function getWeather() {
+  const city = document.getElementById("city").value;
+  const output = document.getElementById("output");
+
+  const API_KEY = "https://api.openweathermap.org/data/2.5/weather";
+
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city + "&units=metric&appid=" + API_KEY;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+
+  xhr.onload = function () {
+    if (xhr.status !== 200) {
+      output.innerText = "Error: " + xhr.status;
+      return;
+    }
+
+    const data = JSON.parse(xhr.responseText);
+
+    output.innerText =
+      "City: " + data.name + "\n" +
+      "Temperature: " + data.main.temp + " °C\n" +
+      "Humidity: " + data.main.humidity + "%\n" +
+      "Condition: " + data.weather[0].description;
+  };
+
+  xhr.onerror = function () {
+    output.innerText = "Network error";
+  };
+
+  xhr.send();
+}";
 
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
